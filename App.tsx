71aaa1bd6d1d0e3b5d256021ac817e2cb6570b03
Kyle Darren <laguertakyledarren@gmail.com>
2024-10-31 21:12:07 +0800

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ScrollView, Button } from "react-native";
+import { useState } from "react";
+
+const img_src = require("./assets/adaptive-icon.png");
+
 
 export default function App() {
+  const [txt_color, setColor] = useState("white");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Image source={img_src} style={{ width: 100, height: 100}}/> */}
+      <Image source={{ uri: "https://picsum.photos/200/300" }} style={{ height: 200, width: 300 }}/>
+      <Text style={{ fontSize: 50 }}>
+        <Text style={{ color: txt_color }}>B</Text>ud
+        <Text style={{ color: txt_color }}>H</Text>ub
+      </Text>
+      <Button title="Start" onPress={() => setColor("black")} color="midnightblue"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fa0",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
